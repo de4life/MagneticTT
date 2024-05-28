ef2493938e0a540cb322b44c30a500955b856bc8
Artur Ageev <artur.tasher@gmail.com>
2024-05-29 00:31:27 +0300

    add deviceCell

---
+++ MagneticTT/Views/DeviceCell.swift
@@ -0,0 +1,42 @@
+//
+//  DeviceCell.swift
+//  MagneticTT
+//
+//  Created by Artur Ageev on 28.05.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct DeviceCell: View {
+    let wifiName: String
+    let ipAddress: String
+    let wifiIcon: String
+
+    var body: some View {
+        HStack {
+            Image(uiImage: UIImage(named: wifiIcon)!)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color(red: 0.063, green: 0.051, blue: 0.173))
+                .cornerRadius(10)
+
+            VStack(alignment: .leading) {
+                Text(wifiName)
+                    .foregroundColor(.white)
+                Text(ipAddress)
+                    .foregroundColor(.gray)
+            }
+            .padding(.leading, 10)
+
+            Spacer()
+
+            Image(systemName: "chevron.right")
+                .foregroundColor(.white)
+                .padding()
+        }
+        .padding()
+        .background(Color(red: 0.063, green: 0.051, blue: 0.173))
+        .cornerRadius(10)
+    }
+}
