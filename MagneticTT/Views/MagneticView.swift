37a0d298327be404e87b99e81473437bfb667855
Artur Ageev <artur.tasher@gmail.com>
2024-05-28 14:16:41 +0300

    update magneticview

--- MagneticTT/Views/MagneticView.swift
+++ MagneticTT/Views/MagneticView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct MagneticView: View {
     @Environment(\.presentationMode) var presentationMode
-    
+    @State private var isArrowMoving = false
+    @State private var searchText = "Search checking"
+    @State private var buttonText = "Search"
+    @State private var timer: Timer?
+    @State private var currentRotation: Double = 0
+
     var body: some View {
         createViewUI()
-            
             .toolbar {
-                
-                
                 ToolbarItem(placement: .principal) {
                     Text("Magnetic Detection")
                         .multilineTextAlignment(.center)
@@ -23,13 +25,13 @@ struct MagneticView: View {
                 }
             }
     }
-    
+
     private func createViewUI() -> some View {
         GeometryReader { geo in
             ZStack {
                 Color.black
                     .edgesIgnoringSafeArea(.all)
-                
+
                 VStack {
                     ZStack {
                         Image("magneticview")
@@ -37,11 +39,48 @@ struct MagneticView: View {
                             .frame(width: 390, height: 329)
                             .offset(y: -30)
                     }
+
+                    VStack {
+                        SpeedometerView(isArrowMoving: $isArrowMoving, currentRotation: $currentRotation)
+                            .offset(y: -30)
+                    }
+                    VStack {
+                        Text(searchText)
+                            .foregroundColor(.white)
+                    }
                     Spacer()
-                    
-                    
+                    Button(action: {
+                        isArrowMoving.toggle()
+                        if isArrowMoving {
+                            startSearch()
+                        } else {
+                            stopSearch()
+                        }
+                    }) {
+                        Text(buttonText)
+                            .frame(width: 350, height: 50)
+                            .background(Color(red: 0.427, green: 0.349, blue: 0.827))
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                    Spacer()
                 }
             }
         }
+    }
+
+    private func startSearch() {
+        buttonText = "Stop"
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            let value = Int.random(in: 1...100)
+            searchText = "\(value) µT"
+            currentRotation = Double(value) / 100 * 180
+        }
+    }
+
+    private func stopSearch() {
+        buttonText = "Search"
+        timer?.invalidate()
+        timer = nil
     }
 }
